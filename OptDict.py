e5d67bd1e46b7911505e205f939c507bef3bea71
oas <omar.agudo@gmail.com>
2017-01-31 22:49:17 +0100

    Add brief description

--- OptDict.py
+++ OptDict.py
@@ -4,7 +4,7 @@ import copy
 class OptDict(defaultdict):
     """ Recursive default dictionary definition allowing to traverse it based 
         on optional (potential missing) keys. Useful for traversing JSON with 
-        potential missing keys
+        missing keys
     """
     
     def __missing__(self, key):
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # optional-dict
+
+Recursive default dictionary definition allowing to traverse it based on optional (potential missing) keys. Useful for traversing JSON with missing keys
+
+print opt_d["key_2"]["missing_key"].get("key_2_1_1", "default_value")
